8aa1796a9bfa1bf14afaaed65a388715157bc59b
Diego Moura <programmercodeoficial@gmail.com>
2024-05-30 11:44:58 -0300

    Initial setup

---
+++ olabiflix/src/main/java/br/diegomoura/olabiflix/OlabiflixApplication.java
@@ -0,0 +1,13 @@
+package br.diegomoura.olabiflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlabiflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlabiflixApplication.class, args);
+	}
+
+}
---
+++ olabiflix/src/test/java/br/diegomoura/olabiflix/OlabiflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.diegomoura.olabiflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlabiflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
