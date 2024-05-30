44a5209ca8081091d3220c17541a0780b70be098
Diego Moura <programmercodeoficial@gmail.com>
2024-05-30 12:31:33 -0300

    Testing Requisition with @RestController

--- olabiflix/src/main/java/br/diegomoura/olabiflix/OlabiflixApplication.java
+++ olabiflix/src/main/java/br/diegomoura/olabiflix/OlabiflixApplication.java
@@ -1,13 +1,35 @@
 package br.diegomoura.olabiflix;
 
+import br.diegomoura.olabiflix.model.entity.Movie;
+import br.diegomoura.olabiflix.repository.MovieRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class OlabiflixApplication {
 
-	public static void main(String[] args) {
+	private final MovieRepository repository;
+
+    public OlabiflixApplication(MovieRepository repository) {
+        this.repository = repository;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(OlabiflixApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello Olabi ;)";
+	}
+
+	@GetMapping("/movies")
+	public List<Movie> getMovies(){
+		return repository.findAll();
+	}
 }
