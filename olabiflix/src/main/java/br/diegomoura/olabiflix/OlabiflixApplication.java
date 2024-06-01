d7449ac390afbab664cb25e26201480c8f8ec6c2
Diego Moura <programmercodeoficial@gmail.com>
2024-06-01 11:50:52 -0300

    Creating route movies/create

--- olabiflix/src/main/java/br/diegomoura/olabiflix/OlabiflixApplication.java
+++ olabiflix/src/main/java/br/diegomoura/olabiflix/OlabiflixApplication.java
@@ -7,6 +7,8 @@ import br.diegomoura.olabiflix.repository.SerieRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -35,6 +37,11 @@ public class OlabiflixApplication {
 	@GetMapping("/movies")
 	public List<Movie> getMovies(){
 		return movieRepository.findAll();
+	}
+
+	@PostMapping("/movies/create")
+	public Movie create(@RequestBody Movie movieBody) {
+		return movieRepository.save(movieBody);
 	}
 
 	@GetMapping("/series")
